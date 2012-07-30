0f9ce4f98b3afd7e0979611a5c3a8479071164b0
Peter Sergeant <pete@clueball.com>
2012-07-30 22:10:20 +0100

    - Rewrite to use Data.Map instead of home-grown implementation - Function composition and helper functions make it much more concise

--- DoubleMetaphone.hs
+++ DoubleMetaphone.hs
@@ -2,140 +2,169 @@
 module Text.DoubleMetaphone (doubleMetaphone, doubleMetaphoneDebug)
 where
 
-
-import Data.List
+import Prelude
+import Data.Map
 import Data.Char
 
-data HVal = HInt Int | HStr String deriving (Eq, Ord, Show, Read)
-instance Num HVal where
-    HInt a + HInt b = HInt (a + b)
-    fromInteger = fromInteger
+-- Temporary test cases go here, until it's time to split them out
+runTests :: [(String,[String],[String])]
+runTests =  Prelude.map (\x -> ((fst x),(snd x),(doubleMetaphone (fst x)))) (
+            Prelude.filter (\x -> (doubleMetaphone (fst x)) /= (snd x) ) [
+               ("Edge",  ["AJ"]),
+               ("X",     ["S"]),
+               ("BB",    ["P"]),
+               ("Canary",["Canard"] )
+            ])
+-- End tests...
 
-type SHash = [(String, HVal)]
+-- This holds the current state of the world
+type Context = (Map String Int, Map String String)
+defaultContext =  (
+                    -- Integer values
+                    fromList([
+                      ( "skip",     0 :: Int ),
+                      ( "position", 0 :: Int )
+                    ]),
+                    -- String values
+                    fromList([
+                      ( "primary",   "" ),
+                      ( "secondary", "" ),
+                      ( "original",  "" )
+                    ])
+                  )
 
+--
+-- These are the interface functions that we actually use to interact with the
+-- context
+--
+
+-- Skip the next input letter
+skip :: Context -> Context
+skip context = cInc "skip" 1 context
+
+-- Add a string to primary and secondary outputs
+add :: String -> Context -> Context
+add str context = addPrimary str (addSecondary str context)
+
+-- Add a string to just the primary output
+addPrimary :: String -> Context -> Context
+addPrimary str context = cApp "primary" str context
+
+-- Add a string to just the secondary output
+addSecondary :: String -> Context -> Context
+addSecondary str context = cApp "secondary" str context
+
+isVowel :: Char -> Bool
+isVowel x = x `elem` "AEIOUY"
+
+--
+-- These are the helper functions for the interface functions above
+--
+
+-- Increment one of the integer values
+cInc :: String -> Int -> Context -> Context
+cInc key increment context = (adjust (+ increment) key (fst context), snd context)
+
+-- Append to a string value
+cApp :: String -> String -> Context -> Context
+cApp key value context = (fst context, (adjust (++ value) key (snd context)) )
+
+-- Lookup an integer from the context
+cLookupInt :: String -> Context -> Int
+cLookupInt key context = findWithDefault (error "Unknown int") key (fst context)
+
+-- Lookup a string from the context
+cLookupStr :: String -> Context -> String
+cLookupStr key context = findWithDefault (error "Unknown str") key (snd context)
+
+--
+-- Here is our external interface
+--
+
+-- Return a list of one or two strings with the metaphone equivalent(s) of the
+-- input string.
 doubleMetaphone :: String -> [String]
 doubleMetaphone input = if primary /= secondary && (length secondary) > 0
                           then [primary,secondary]
                           else [primary]
                         where context   = doubleMetaphoneDebug input
-                              primary   = hStr (hLookup "primary" context)
-                              secondary = hStr (hLookup "secondary" context)
+                              primary   = cLookupStr "primary"   context
+                              secondary = cLookupStr "secondary" context
 
-doubleMetaphoneDebug :: String -> SHash
-doubleMetaphoneDebug input = conc (map (toUpper) input) [
-                          ("primary",   HStr ""),
-                          ("secondary", HStr ""),
-                          ("skip",      HInt 0 ),
-                          ("original",  HStr input ),
-                          ("position",  HInt 0 )
-                        ]
+-- Run the algorithm, but return the whole context instead of just the hashed
+-- strings
+doubleMetaphoneDebug :: String -> Context
+doubleMetaphoneDebug input = m (Prelude.map (toUpper) input) defaultContext
 
-conc :: String -> SHash -> SHash
-conc [] c = c
-conc full@(x:xs) context
+-- This is the recursive function that does the transform. We pull out some
+-- very useful pieces of data in the 'where' clause at the bottom, and it's
+-- between those and the interface functions above where we do the magic...
+m :: String -> Context -> Context
+m [] c = c
+m full@(x:xs) context -- Use 'c', not 'context', as per the 'where' section
 
--- Meta-cases for managing the recursion
-  -- Keep going while either of our accumlators are smaller than four
-  | (length primary) > 3 || (length secondary) > 3 = context
+  -- When we've found enough characters, we're all done
+  | any (\s -> (length (cLookupStr s context) > 3) ) [ "primary", "secondary" ] = c
 
   -- Skip over characters if we're meant to
-  | skip > 0 = conc xs (hInc "skip" (0-1) c)
+  | toSkip > 0 = next (cInc "skip" (0-1))
 
--- Things that occur at the beginning of a name
   -- There are a funny set of consonants at the beginning of names where we want
   -- to pretend we didn't see the first letter. Matching without doing anything
   -- does that
-  | isStart && x2 `elem` ["GN", "KN", "PN", "WR", "PS"]
-    = conc xs c
+  | isStart && xx `elem` ["GN", "KN", "PN", "WR", "PS"] = next (id)
+
   -- Initial 'X' is pronounced 'Z' e.g. 'Xavier'
-  | isStart && x == 'X'
-    = conc xs (addS "S" c)
+  | isStart && x == 'X' = next (add "S")
 
   -- Match any starting vowels, and translate them directly to A
-  | isStart && isVowel x
-    = conc xs (addS "A" c)
+  | isStart && isVowel x = next (add "A")
 
--- Everything else...
   -- B
-  | x2 == "BB" = conc xs (hInc "skip" 1 (addS "P" c))
-  | x  == 'B'  = conc xs (addS "P" c)
+  | xx == "BB" = next (add "P" . skip) -- BB -> P
+  | x  == 'B'  = next (add "P")        -- B  -> P
+
+  -- Combinations like 'edge'
+  | xxx `elem` ["DGI","DGE","DGY"] = next (add "J" . skip . skip)
+
+  -- Combinations like 'edgar'
+  | xx == "DG" = next (add "TK" . skip)
 
   -- D
-  | x2 == "DG" && x3 `elem` ["DGI","DGE","DGY"] -- edge
-    = conc xs (hInc "skip" 2 (addS "J" c))
-  | x2 == "DG" -- edgar
-    = conc xs (hInc "skip" 1 (addS "TK" c))
-  | x2 == "DD" || x2 == "DT"
-    = conc xs (hInc "skip" 1 (addS "T" c))
-  | x == 'D'
-    = conc xs (addS "T" c)
+  | (xx == "DD" || xx == "DT") = next (add "T" . skip)
+  | x == 'D' = next (add "T")
 
-  -- F
-  | x2 == "FF"
-    = conc xs (hInc "skip" 1 (addS "F" c))
-  | x  == 'F'
-    = conc xs (addS "F" c)
-
-  -- H - only show it if it's at the start of between two vowels
-  -- | ( x == 'H' && isStart ) || (isVowel xp1 && isVowel xm1)
-  --  = conc xs (hInc "skip" 1 (addS "H" c))
+  -- -- F
+  -- | x2 == "FF"
+  --  = conc xs (hInc "skip" 1 (addS "F" c))
+  -- | x  == 'F'
+  --  = conc xs (addS "F" c)
 
   -- Default case, do nothing!
-  | otherwise = conc xs (hApp "primary" ['*',x] c)
+  | otherwise = next (add "*")
 
 -- Here we unpack the context
-        -- We build up the main and alternative spelling in these
-  where primary   = hStr (hLookup "primary"   context)
-        secondary = hStr (hLookup "secondary" context)
-        -- How many letters to skip
-        skip      = hInt (hLookup "skip"      context)
-        -- The original string for reference
-        original  = hStr (hLookup "original"  context)
+  where
+        -- Number of letters we're meant to be skipping
+        toSkip = cLookupInt "skip" context
+
+        -- The original string, for reference
+        original = cLookupInt "original" context
+
         -- The current position
-        position  = hInt (hLookup "position"  context)
+        position = cLookupInt "position" context
+
         -- Create a copy of the context where the position is already
         -- incremented
-        c         = hInc "position" 1 context
+        c         = cInc "position" 1 context
+
         -- Some shortcuts to make our life a little easier
-        x2        = take 2 full -- this character and the next
-        x3        = take 3 full -- this character and the next two
+        xx        = take 2 full -- this character and the next
+        xxx       = take 3 full -- this character and the next two
         -- xm1       = if (position == 0) then "" else original !! (position - 1)
         xp1       = take 1 xs -- The next character, alone
         isStart   = position == 0
 
-isVowel :: Char -> Bool
-isVowel x = x `elem` "AEIOUY"
+        -- Apply the context to whatever expression we were given, and recurse
+        next = ( \expression -> (m xs (expression c)) )
 
-hInt (HInt a) = a
-hStr (HStr a) = a
-
--- Add a letter to primary and secondary
-addS :: String -> SHash -> SHash
-addS x c = (hApp "primary" x (hApp "secondary" x c))
-
--- Join two hstrings
-hStrConcat :: HVal -> HVal -> HVal
-hStrConcat (HStr a) (HStr b) = HStr (a ++ b)
-
--- Combine two hashes
-hMerge :: SHash -> SHash -> SHash
-hMerge left right = unionBy (\x y -> (fst x) == (fst y)) right left
-
--- Short-hand to setting a single value in the hash
-hSet :: String -> HVal -> SHash -> SHash
-hSet key val from = hMerge from [(key, val)]
-
--- Increment an HVal in the hash
-hInc :: String -> Int -> SHash -> SHash
-hInc key val from = hSet key ((HInt val) + (hLookup key from)) from
-
--- Add to an HStr in the hash
-hApp :: String -> String -> SHash -> SHash
-hApp key val from = hSet key (hStrConcat (hLookup key from) (HStr val)) from
-
--- This implementation requires that the key actually exist, or it'll throw,
--- but that's good enough for us, and no messin' about with Maybe
-hLookup :: String -> SHash -> HVal
-hLookup key (x:xs)
-    | fst(x) == key = snd(x)
-    | otherwise = (hLookup key xs)
